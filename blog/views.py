50019e09209b33fb34cddd01b1aec714e3b617b7
Shreyansh <shreyansh.pettswood@gmail.com>
2017-03-22 00:28:23 +0530

    Added view to html file

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
